a56700f8b9134c32f673d9c0646b846fb35215ca
armdong <armdong@163.com>
2020-04-07 17:15:33 +0800

    set up enviroument

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Material-UI demos",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
